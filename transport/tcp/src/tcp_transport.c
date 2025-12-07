2d7879e546108ffb33b89d454b7517b0c333c70b
vedarsh reddy muniratnam <71834823+vedarsh@users.noreply.github.com>
2025-12-07 15:09:48 +0400

    removed unneeded function

--- transport/tcp/src/tcp_transport.c
+++ transport/tcp/src/tcp_transport.c
@@ -28,12 +28,6 @@ static int set_blocking(int fd) {
     return fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
 }
 
-static int set_nonblocking(int fd) {
-    int flags = fcntl(fd, F_GETFL, 0);
-    if (flags == -1) return -1;
-    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-}
-
 static void set_tcp_nodelay(int fd) {
     int opt = 1;
     setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt));
